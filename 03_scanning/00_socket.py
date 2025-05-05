9b3199ae31f7d65f02ae320c3725ecf07c106e28
donadelden <denisdonadel@gmail.com>
2025-05-05 16:20:57 +0200

    add socket example

---
+++ 03_scanning/00_socket.py
@@ -0,0 +1,16 @@
+import socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+sock.bind(('', 1337))
+
+# listen for max = 5 connections
+sock.listen(5)
+
+# accept a connection
+print("Waiting for a connection...")
+sock, addr = sock.accept()
+# print info about the connection
+print("Got connection from", addr)
+
+# close the socket
+sock.close()
